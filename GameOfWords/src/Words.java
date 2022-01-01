09d4bf90c7903482026ebb96ecd75a6128f1c7b1
Laura-Xia <lanxuanxia@stmarksschool.org>
2022-01-01 16:52:02 -0500

    Introductory scene finished

--- GameOfWords/src/Words.java
+++ GameOfWords/src/Words.java
@@ -32,25 +32,79 @@ public class Words extends JPanel implements KeyListener {
 
 	
 	// your instance variables here, I've given you a few 
-	private boolean up1, down1, up2, down2; 		// booleans to keep track of paddle movement
+	private boolean collide = false; 		// booleans to keep track of collision
 	private boolean solo = false;
-	private int[] count = new int[10];
+	private int[] count = new int[10];// counter for type writer effect 
+	private boolean[] countStart = new boolean[10];// booleans to decide if the next string can be printed
+	private String[] text = new String[10];// the texts
+	private boolean next = false;// the boolean to decide if the program should progress to the next string
+	private int counter = 0;// count which string the program is on
+	private int[] timer  = new int[10];// timer in between the strings
+	private int[] x = new int[10];// x positions of the strings
+	private int[] y = new int[10];// y positions of the strings
+	private boolean move = false;
+	private int countDoor = 0;// counter for the door image.
 	// image variables
-	private Image I;
+	private Image meImg, doorImg, door2Img, door3Img;
+	private int Ix = 51;
+	private int Iy = 116;
 	
 	// this method returns the "inter" for the type writer effect
 	public String type(String str, int num) {
 		int inter = num/10;
 		if (inter>str.length()) {
+			next = true;
 			return str;
 		}
 		else {
 			return str.substring(0, inter);
 		}
 	}
-	// upload my images
+	// upload my images and give values to arrays
 	public void setup() {
-		I = Toolkit.getDefaultToolkit().getImage("I.png");
+		// upload my images
+		meImg = Toolkit.getDefaultToolkit().getImage("I.png");
+		doorImg = Toolkit.getDefaultToolkit().getImage("door.png");
+		door2Img = Toolkit.getDefaultToolkit().getImage("door 2.png");
+		door3Img = Toolkit.getDefaultToolkit().getImage("door 3.png");
+		
+		
+		// text array
+		text[0] = " wake up to complete darkness in my room.";
+		text[1] = "The eeriness is making me uncomfortable, I";
+		text[2] = "decide to leave the room.";
+		text[3] = "But where is the door?";
+		
+		// set first countStart boolean in order to initiate the typing
+		countStart[0] = true;
+		
+		// time between each string
+		if (timer[1]==60) {
+			countStart[1] = true;
+		}
+		if (timer[2]==20) {
+			countStart[2] = true;
+		}
+		if (timer[3]==70) {
+			countStart[3] = true;
+		}
+		
+		// setting up x and y positions for the strings
+		x[0] = 68;
+		y[0] = 140;
+		x[1] = 50;
+		y[1] = 220;
+		x[2] = 50;
+		y[2] = 300;
+		x[3] = 50;
+		y[3] = 420;
+	}
+	
+	// check if I and something else collides
+	public void collision() {
+		if (Ix>=242&&Ix<=291&&Iy>=401&&Iy<=421) {
+			collide = true;
+		}
 	}
 	// defines what we want to happen anytime we draw the game
 	// you simply need to fill in a few parameters here
@@ -60,15 +114,44 @@ public class Words extends JPanel implements KeyListener {
 		g.setColor(Color.black);
 		g.fillRect(0, 0, WIDTH, HEIGHT);
 		
+		g.drawImage(meImg, Ix, Iy, 17, 31, null);
+		
 		// draw your rectangles and circles here
 		// .......
 		Font f = new Font("Chalkboard", Font.PLAIN, 24);
 		g.setFont(f);
 		g.setColor(Color.white);
-		g.drawImage(I, 50, 160, 32, 32, this);
-//		count[0]++;
-//		g.drawString(type(" woke up to complete darkness in my room", count[0]), 68, 160);
-		// writes the score of the game - you just need to fill the scores in
+		if (countStart[counter]==true) {
+			count[counter]++;
+			g.drawString(type(text[counter], count[counter]), x[counter], y[counter]);
+		}
+		else {
+			timer[counter]++;
+		}
+		if (next==true) {
+			counter++;
+			next = false;
+		}
+		for (int i = 0; i<counter; i++) {
+			if (i==3) {
+				text[3] = "But where is the      ?";
+				if(countDoor<20) g.drawImage(doorImg, 242, 401, 49, 20, null);
+				else if(countDoor<40) g.drawImage(door2Img, 242, 401, 49, 20, null);
+				else if (countDoor<60) g.drawImage(door3Img, 242, 401, 49, 20, null);
+			}
+			g.drawString(text[i], x[i], y[i]);
+		}
+		if (counter==4) move = true;
+		// if collide, clear screen
+		if(collide == true) {
+			countDoor++;
+			// background color is black
+			if (countDoor >= 80) {
+				g.setColor(Color.black);
+				g.fillRect(0, 0, WIDTH, HEIGHT);
+				g.drawImage(meImg, Ix, Iy, 17, 31, null);
+			}
+		}
 	}
 
 	// defines what we want to happen if a keyboard button has 
@@ -78,25 +161,25 @@ public class Words extends JPanel implements KeyListener {
 		int keyCode = e.getKeyCode();
 		
 		// changes paddle direction based on what button is pressed
-		if (keyCode == KeyEvent.VK_DOWN) 
+		if (keyCode == KeyEvent.VK_DOWN && move==true) {
+			Iy+=32;
+		}
 			// fill this in
 		
-		if (keyCode == KeyEvent.VK_UP) 
+		if (keyCode == KeyEvent.VK_UP && move == true) {
+			Iy-=32;
+		} 
+		
+		if (keyCode == KeyEvent.VK_LEFT && move==true) {
+			Ix-=32;
+		}
+			// fill this in
+		
+		if (keyCode == KeyEvent.VK_RIGHT && move == true) {
+			Ix+=32;
+		} 
 			// fill this in
 
-		if (e.getKeyChar() == 'w')
-			// move paddle down
-		
-		if (e.getKeyChar() =='s')
-			// fill this in
-			
-		// turn 1-player mode on
-		if (e.getKeyChar() == '1')
-			// fill this in
-			
-		// turn 2-player mode on
-		if (e.getKeyChar() == '2')
-			// fill this in
 	}
 
 	// defines what we want to happen if a keyboard button
@@ -139,6 +222,8 @@ public class Words extends JPanel implements KeyListener {
 		while (true) {
 	
 			// draws the game
+			setup();
+			collision();
 			repaint();
 			
 			
